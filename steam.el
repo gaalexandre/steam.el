809149e5a6f7471ae422fb43f7ac6f7ebf562d7a
alexandre <aran.alexandre@gmail.com>
2022-02-06 02:15:12 +0100

    Fix steam-check-xml-response's docstring
    It was longer than 80 characters and generated a warning, spiting it
    into two lines fixes the issue.

--- steam.el
+++ steam.el
@@ -51,7 +51,8 @@
   :group 'steam)
 
 (defun steam-check-xml-response (xml)
-  "Check XML from steam for errors, return an error message if an error was detected, else nil."
+  "Check XML from steam for errors.
+Return an error message if an error was detected, else nil."
   (let ((error-node (xml-get-children xml 'error))
         (games-node (xml-get-children xml 'games)))
     (cond
